753198c268636488e819bb3fdfd0d4c2f5c10727
thanatos <seniaslams@gmail.com>
2022-04-21 16:56:18 +0500

    Update cli

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
-export const askAboutName = () => {
-  const name = readlineSync.question("May I have your name? ");
+const askAboutName = () => {
+  const name = readlineSync.question('May I have your name? ');
   return name;
 };
+
+export default askAboutName;
