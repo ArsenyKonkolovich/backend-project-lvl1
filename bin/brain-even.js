855d51cf075f20bb449dfff65aacde9ead416ba1
thanatos <seniaslams@gmail.com>
2022-04-22 16:44:23 +0500

    Update brain-even.js

---
+++ bin/brain-even.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/no-named-as-default */
+
+/* eslint-disable import/extensions */
+
+import readlineSync from 'readline-sync';
+import isEven from '../src/isEven.js';
+import getRandomInt from '../src/generateRandomNumb.js';
+
+// minimum number, use only integer number
+const minNumb = 1;
+// maximum number, use only integer number
+const maxNumb = 100;
+
+// const randomNumb = getRandomInt(minNumb, maxNumb);
+let name = '';
+const maximumTrueAnswers = 3;
+
+const askName = () => {
+  name = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${name}!`);
+};
+
+console.log('Welcome to the Brain Games!');
+askName();
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+const gameEngine = (answerCount) => {
+  const randomNumb = getRandomInt(minNumb, maxNumb);
+  if (answerCount === 0) {
+    console.log(`Congratulations, ${name}!`);
+    return true;
+  }
+  let counter = answerCount;
+  const answer = readlineSync.question(`Question: ${randomNumb}--> `);
+  console.log(`Your answer: ${answer}!`);
+  if (isEven(randomNumb) === 'yes' && answer === 'yes') {
+    counter -= 1;
+    console.log('Correct!');
+    return gameEngine(counter);
+  }
+  if (isEven(randomNumb) === 'no' && answer === 'no') {
+    counter -= 1;
+    console.log('Correct!');
+    return gameEngine(counter);
+  }
+  console.log(`Uh, no! Is wrong answer;(.
+    Correct answer was ${isEven(randomNumb)}. Let 's try again, ${name}!!`);
+  return true;
+};
+
+gameEngine(maximumTrueAnswers);
