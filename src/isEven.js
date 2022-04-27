51e999fb0b396c05175a4294477f66988761fcaf
thanatos <seniaslams@gmail.com>
2022-04-27 21:14:39 +0500

    Delete isEven.js

--- src/isEven.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-/* eslint-disable import/extensions */
-
-const getEven = (number) => number % 2 === 0;
-
-const isEven = (n) => (getEven(n) ? 'yes' : 'no');
-
-export default isEven;
