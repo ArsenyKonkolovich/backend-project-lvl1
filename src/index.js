9ba1e6ba12e44169d33888df678f2fb7e8cbe2d8
thanatos <seniaslams@gmail.com>
2022-04-27 21:09:40 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,64 +1,27 @@
-/* eslint-disable import/no-named-as-default */
 import readlineSync from 'readline-sync';
-import * as hi from './cli.js';
-// eslint-disable-next-line import/no-cycle
-// eslint-disable-next-line import/no-named-as-default
-// eslint-disable-next-line import/no-named-as-default-member
 
-// eslint-disable-next-line import/prefer-default-export
-export const sayWelcomeAndName = () => {
+const rounds = 3;
+
+const gameEngine = (startGame, rules) => {
   console.log('Welcome to the Brain Games!');
-  const name = hi.askAboutName();
-  return name;
-};
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(rules);
 
-const addition = (name, num1, num2) => {
-  const answer = readlineSync.question(`Question: ${num1} + ${num2}--> `);
-  console.log(`Your answer: ${answer}!`);
-  const result = num1 + num2;
-  if (Number(answer) === result) {
+  for (let i = 0; i < rounds; i += 1) {
+    const [question, correctAnswer] = startGame();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer !== correctAnswer) {
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
+      );
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
     console.log('Correct!');
-    return true;
   }
-  console.log(`${answer} is wrong answer ;(. Correct answer was ${result}. Let's try again, ${name}!`);
-  return false;
+  console.log(`Congratulations, ${userName}!`);
 };
 
-const subtraction = (name, num1, num2) => {
-  const result = num1 - num2;
-  const answer = readlineSync.question(`Question: ${num1} - ${num2}--> `);
-  console.log(`Your answer: ${answer}!`);
-  if (Number(answer) === result) {
-    console.log('Correct!');
-    return true;
-  }
-  console.log(`${answer} is wrong answer ;(. Correct answer was ${result}. Let's try again, ${name}!`);
-  return false;
-};
-
-const multiplication = (name, num1, num2) => {
-  const result = num1 * num2;
-  const answer = readlineSync.question(`Question: ${num1} * ${num2}--> `);
-  console.log(`Your answer: ${answer}!`);
-  if (Number(answer) === result) {
-    console.log('Correct!');
-    return true;
-  }
-  console.log(`${answer} is wrong answer ;(. Correct answer was ${result}. Let's try again, ${name}!`);
-  return false;
-};
-const arrOfOperation = [addition, subtraction, multiplication];
-
-export const getRandomOperation = (name, num1, num2) => {
-  // eslint-disable-next-line max-len
-  const result = arrOfOperation[Math.floor(Math.random() * arrOfOperation.length)](name, num1, num2);
-  return result;
-};
-
-export const gcd = (num1, num2) => {
-  if (!num2) {
-    return num1;
-  }
-
-  return gcd(num2, num1 % num2);
-};
+export default gameEngine;
