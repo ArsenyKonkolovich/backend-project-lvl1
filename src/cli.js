13e1fa93c04b44b4c65f9e3d6d08562297145467
thanatos <seniaslams@gmail.com>
2022-04-28 13:34:53 +0500

    Update cli.js rev#1

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const askAboutName = () => {
   const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${name}`);
+  console.log(`Hello, ${name}`);
 };
 
 export default askAboutName;
