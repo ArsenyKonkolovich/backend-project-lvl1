6bcd39d0f5105f6651291b2d174c85f801a02c72
thanatos <seniaslams@gmail.com>
2022-04-28 14:13:33 +0500

    Update generateRandomNumb.js

--- src/generateRandomNumb.js
+++ src/generateRandomNumb.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-/* eslint-disable import/extensions */
-
 const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min + 1;
 
 export default getRandomInt;
