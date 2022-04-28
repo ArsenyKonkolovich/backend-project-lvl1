7ddbce840a00d46959baab1ed2166e81c0aae1d7
thanatos <seniaslams@gmail.com>
2022-04-28 18:50:39 +0500

    Update index rev#1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const rounds = 3;
+
 const gameEngine = (startGame, rules) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
