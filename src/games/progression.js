24ae55c6316055973936344ed8cbb8e12dc6a1b0
thanatos <seniaslams@gmail.com>
2022-05-02 15:14:00 +0500

    Update progression.js rev#2

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,27 +2,26 @@ import getRandomInt from '../generateRandomNumb.js';
 import startGame from '../index.js';
 
 const rules = 'What number is missing in the progression?';
+const countOfRound = 10;
 
-const generateProgression = () => {
-  const firstNumber = getRandomInt(-10, 10);
-  const step = getRandomInt(1, 10);
-
-  const arrOfNum = [];
-  arrOfNum[0] = firstNumber;
-  for (let i = 1; i < 10; i += 1) {
-    arrOfNum[i] = arrOfNum[i - 1] + step;
+const generateProgression = (firstNumber, step) => {
+  const progression = [];
+  for (let i = 0; i < countOfRound; i += 1) {
+    progression.push(firstNumber + (step * i));
   }
 
-  const missingEleomIndex = getRandomInt(0, arrOfNum.length - 1);
-  const missingElem = arrOfNum[missingEleomIndex];
-  arrOfNum[missingEleomIndex] = '..';
+  const missingEleomIndex = getRandomInt(0, progression.length - 1);
+  const missingElem = progression[missingEleomIndex];
+  progression[missingEleomIndex] = '..';
 
-  return [arrOfNum, missingElem];
+  return [progression, missingElem];
 };
 
 const generateRound = () => {
-  const [arrOfNum, missingElem] = generateProgression();
-  const question = arrOfNum.join(' ');
+  const firstNumber = getRandomInt(-10, 10);
+  const step = getRandomInt(1, 10);
+  const [progression, missingElem] = generateProgression(firstNumber, step);
+  const question = progression.join(' ');
   const correctAnswer = missingElem;
   return [question, String(correctAnswer)];
 };
