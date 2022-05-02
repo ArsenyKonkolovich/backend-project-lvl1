245add3c52db473a6c8e0cf0adb7a7432e198dc3
thanatos <seniaslams@gmail.com>
2022-05-02 13:52:48 +0500

    Update even.js rev#2

--- src/games/even.js
+++ src/games/even.js
@@ -1,11 +1,11 @@
 import getRandomInt from '../generateRandomNumb.js';
 import gameEngine from '../index.js';
 
-const isEven = (number) => number % 2 === 0;
 const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
-
 const min = 0;
 const max = 100;
+
+const isEven = (number) => number % 2 === 0;
 
 const startGame = () => {
   const question = getRandomInt(min, max);
